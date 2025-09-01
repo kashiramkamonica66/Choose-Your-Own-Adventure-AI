36ee9f34f7873a5cab8bf9ee8f75590f03666173
Monica Kashiramka <monica.kashiramka@cielotalent.com>
2025-09-01 21:04:30 +0530

    update backend for openai service

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -1,6 +1,7 @@
 from sqlalchemy import false
 from sqlalchemy.orm import Session
 # from core.config import settings
+import os
 
 
 from langchain_openai import ChatOpenAI
@@ -25,6 +26,12 @@ class StoryGenerator:
 
     @classmethod
     def _get_llm(cls):
+        openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
+        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
+
+        if openai_api_key and serviceurl:
+            return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
+
         return ChatOpenAI(model="gpt-4o-mini")
 
     @classmethod
